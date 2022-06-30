570b68498afcbd9b2ac83249dca38a25698e536f
byeongjin <qudwls7468@gmail.com>
2022-06-30 19:25:54 +0900

    add Notice__promotion -- toggleBtn

--- js/main.js
+++ js/main.js
@@ -69,3 +69,18 @@ new Swiper('.promotion .swiper', {
         nextEl: '.promotion .swiper-next',
     },
 });
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+    isHidePromotion = !isHidePromotion 
+    if (isHidePromotion) {
+        // 숨김 처리
+        promotionEl.classList.add('hide');
+    } else {
+        // 보임 처리
+        promotionEl.classList.remove('hide');
+    }
+});
