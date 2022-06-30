4102bf5790d2e6da6446338ca296cc54c6ce0f89
byeongjin <qudwls7468@gmail.com>
2022-06-30 23:33:33 +0900

    add Youtube-iframe -- animation

--- js/main.js
+++ js/main.js
@@ -84,3 +84,26 @@ promotionToggleBtn.addEventListener('click', function () {
         promotionEl.classList.remove('hide');
     }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+    // gsap.to(요소, 시간, 옵션);
+    gsap.to(
+        selector, // 선택자
+        random(1.5, 2.5), // 애니메이션 동작 시간
+        { // 옵션들
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        delay: random(0, delay),
+    });
+}
+floatingObject('.floating', 1, 15);
+floatingObject('.floating', 0.5, 15);
+floatingObject('.floating', 1.5, 20);
