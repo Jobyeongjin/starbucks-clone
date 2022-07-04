92a968d927b82e01182a35664383a147a859322c
byeongjin <qudwls7468@gmail.com>
2022-07-04 23:09:03 +0900

    add ScrollBtn, JS : to-top

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function() {
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () { 
     // _.throttle(함수, 시간), 스크롤 횟수 제한
@@ -27,14 +28,28 @@ window.addEventListener('scroll', _.throttle(function () {
             opacity: 0,
             display: 'none',
         });
+        // 버튼 보이기!
+        gsap.to(toTopEl, .2, {
+            x: 0,
+        });
     } else {
         // 배지 보여주기
         gsap.to(badgeEl, 0.6, {
             opacity: 1,
             display: 'block',
         });
+        // 버튼 숨기기!
+        gsap.to(toTopEl, .2, {
+            x: 100,
+        });
     }
 }, 300));
+
+toTopEl.addEventListener('click', function () {
+    gsap.to(window, .7, {
+        scrollTo: 0,
+    });
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
